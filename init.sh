0f313cd0e463556d11badf8487f63b4b28d5599a
Danil Pismenny <dapi@users.noreply.github.com>
2025-12-29 21:21:48 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,11 +5,6 @@ git submodule update
 
 # Copy .envrc from main/master worktree
 BASE_DIR=$(git worktree list | grep -E '\[(main|master)\]' | head -1 | awk '{print $1}')
-if [ -n "$BASE_DIR" ] && [ -f "$BASE_DIR/.envrc" ]; then
-  cp "$BASE_DIR/.envrc" .envrc
-  echo "Copied .envrc from $BASE_DIR"
-else
-  echo "Warning: Could not find .envrc in main/master worktree"
-fi
+cp "$BASE_DIR/.env*" .
 
 direnv allow
