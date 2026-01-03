c5ffabc6193a02868cfbcd8b095bf28e3eba2895
Danil Pismenny <dapi@users.noreply.github.com>
2026-01-03 16:23:36 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,6 @@ git submodule update
 
 # Copy .envrc from main/master worktree
 BASE_DIR=$(git worktree list | grep -E '\[(main|master)\]' | head -1 | awk '{print $1}')
-cp "$BASE_DIR/.env*" .
+cp $BASE_DIR/.env* .
 
 direnv allow
